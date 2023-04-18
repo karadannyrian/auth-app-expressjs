107e2440a0426a2d4581ba53e0952ade03fe516e
RAKA RIANDANNY <107055208+karadannyrian@users.noreply.github.com>
2023-04-19 00:56:22 +0700

    initial phase

---
+++ api/controllers/userController.js
@@ -0,0 +1,125 @@
+'use strict';
+
+const { ObjectId } = require('bson');
+
+var mongoose = require('mongoose'),
+    jwt = require('jsonwebtoken'),
+    bcrypt = require('bcrypt'),
+    User = mongoose.model('User');
+
+
+const setPayload = (req) => {
+    let payload
+    if (req.body.email) payload = { email: req.body.email }
+    else payload = { username: req.body.username }
+    return payload
+}
+
+exports.register = async (req, res) => {
+    var newUser = new User(req.body);
+    newUser.hash_password = bcrypt.hashSync(req.body.password, 10);
+    try {
+        const response = await newUser.save()
+        if (response) return res.status(201).send({ message: 'SUCCESS' })
+    } catch (error) {
+        return res.status(400).send({ message: error.message })
+    }
+}
+
+exports.signin = async (req, res) => {
+    try {
+        let payload = setPayload(req)
+        const response = await User.findOne(payload)
+        if (!response) {
+            return res.status(401).json({ message: 'Authentication failed. User not found' });
+        }
+        if (!response.comparePassword(req.body.password)) {
+            return res.status(401).json({ message: 'Authentication failed. Invalid password' });
+        }
+        return res.json({ token: jwt.sign({ email: response.email, username: response.username, _id: response._id }, process.env.JWT_SECRET_KEY) });
+    } catch (error) {
+        return res.status(401).json({ message: error?.message });
+    }
+}
+
+exports.loginRequired = function (req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        return res.status(401).json({ message: 'Unauthorized user' });
+    }
+};
+
+exports.profile = function (req, res, next) {
+    if (req.user) {
+        res.send(req.user);
+        next();
+    }
+    else {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+};
+
+exports.getByID = async (req, res) => {
+    try {
+        const response = await User.findOne({ _id: new ObjectId(req.params.id) })
+        if (response) {
+            response.hash_password = undefined
+            response.__v = undefined
+            return res.status(200).json({ data: response });
+        }
+        return res.status(404).json({ message: 'User not found', data: null });
+    } catch (error) {
+        return res.status(404).json({ message: error?.message, data: null });
+    }
+}
+
+exports.updateUser = async (req, res) => {
+    var newUser = new User(req.body);
+    try {
+        let payload = { _id: new ObjectId(req.body.id) }
+        const response = await User.updateOne(payload,
+            {
+                $set: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    updated: newUser.updated
+                },
+            })
+        if (response) {
+            if (response.modifiedCount > 0) return res.status(200).json({ message: 'User updated', data: null });
+            return res.status(404).json({ message: 'Data not found', data: null });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error?.message, data: null });
+    }
+}
+
+exports.changePassword = async (req, res) => {
+    try {
+        let payload = setPayload(req)
+        if (req.body.newPassword === req.body.oldPassword) {
+            return res.status(404).json({ message: "New password invalid", data: null });
+        }
+        let response = await User.findOne(payload)
+        if (!response) {
+            return res.status(401).json({ message: 'User not found' });
+        }
+        if (!response.comparePassword(req.body.oldPassword)) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+        const hash_password = bcrypt.hashSync(req.body.newPassword, 10);
+        response = await User.updateOne(payload,
+            {
+                $set: {
+                    hash_password: hash_password
+                },
+            })
+        if (response) {
+            if (response.modifiedCount > 0) return res.status(200).json({ message: 'Password updated', data: null });
+            return res.status(404).json({ message: 'Data not found', data: null });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error?.message, data: null });
+    }
+}
---
+++ api/routes/userRoutes.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function (app) {
+    var userHandlers = require('../controllers/userController.js');
+    // todoList Routes
+    app.route('/user/getByID/:id')
+        .get(userHandlers.loginRequired, userHandlers.getByID)
+    app.route('/auth/register')
+        .post(userHandlers.register);
+    app.route('/auth/signin')
+        .post(userHandlers.signin);
+    app.route('/auth/updateUser')
+        .put(userHandlers.loginRequired, userHandlers.updateUser);
+    app.route('/auth/changePassword')
+        .post(userHandlers.loginRequired, userHandlers.changePassword);
+};
